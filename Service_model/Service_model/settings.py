feafcc22a9792937a4a02231289c06d0c0d3d7f9
ajit <sajit4862@gmail.com>
2022-07-15 12:07:28 +0530

    First commit

--- Service_model/Service_model/settings.py
+++ Service_model/Service_model/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-al0p=^0w-25yr2_9%+0@3@xb4)lkc(g*!0^40-b*=*u0fr=r9*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','Ajit-env.eba-rtzh4raw.ap-south-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['127.0.0.1','Inverted-env.eba-mfpjibmh.ap-south-1.elasticbeanstalk.com']
 
 
 # Application definition
@@ -80,20 +80,12 @@ WSGI_APPLICATION = 'Service_model.wsgi.application'
 
 
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Inverted',
-        'PASSWORD': '2000',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'root',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-         }
-            }
-}
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
